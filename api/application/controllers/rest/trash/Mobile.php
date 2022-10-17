3325691c211d4e9ef26bcbc48a79ee0c9b3db957
yuliant <masrizal04@gmail.com>
2022-10-17 20:05:47 +0700

    home api fix

--- api/application/controllers/rest/trash/Mobile.php
+++ api/application/controllers/rest/trash/Mobile.php
@@ -21,7 +21,7 @@ class Mobile extends CI_Controller
             die;
         }
 
-        $tempat_sampah = $this->db->query("SELECT * FROM tempat_sampah")->result();
+        $tempat_sampah = $this->db->query("SELECT * FROM tempat_sampah ORDER BY ID_TEMPAT_SAMPAH DESC")->result();
         $respon = [
             'status' => true,
             'message' => "Data berhasil didapatkan",
@@ -51,6 +51,7 @@ class Mobile extends CI_Controller
         $this->db->join('tempat_sampah', 'tempat_sampah.ID_TEMPAT_SAMPAH = list_tugas.ID_TEMPAT_SAMPAH', 'left');
         $this->db->join('mobil_sampah', 'mobil_sampah.ID_MOBIL_SAMPAH = list_tugas.ID_MOBIL_SAMPAH', 'left');
         $this->db->where('list_tugas.ID_PENGGUNA', $id_pengguna);
+        $this->db->order_by("list_tugas.ID_LIST_TUGAS", "desc");
         $tugas = $this->db->get()->result();
 
         $respon = [
@@ -74,7 +75,7 @@ class Mobile extends CI_Controller
             die;
         }
 
-        $mobil_sampah = $this->db->query("SELECT * FROM mobil_sampah")->result();
+        $mobil_sampah = $this->db->query("SELECT * FROM mobil_sampah ORDER BY ID_MOBIL_SAMPAH DESC")->result();
         $respon = [
             'status' => true,
             'message' => "Data berhasil didapatkan",
@@ -96,11 +97,44 @@ class Mobile extends CI_Controller
             die;
         }
 
-        $mobil_sampah = $this->db->query("SELECT * FROM mobil_sampah WHERE status = 'ready'")->result();
+        $mobil_sampah = $this->db->query("SELECT * FROM mobil_sampah WHERE status = 'ready' ORDER BY ID_MOBIL_SAMPAH DESC")->result();
         $respon = [
             'status' => true,
             'message' => "Data berhasil didapatkan",
             'mobil_sampah_ready' => $mobil_sampah
+        ];
+        $json = json_encode($respon);
+        echo $json;
+    }
+
+    public function home()
+    {
+        $api_key = htmlspecialchars($this->input->post('API-KEY'), true);
+        $id_pengguna = htmlspecialchars($this->input->post('id_pengguna'), true);
+
+        $cek_api_key = $this->api_m->CekApiKey($api_key);
+        if ($cek_api_key->num_rows() == 0) {
+            $respon = ['status' => false, 'message' => "Error API Key"];
+            $json = json_encode($respon);
+            echo $json;
+            die;
+        }
+
+        $tempat_sampah = $this->db->query("SELECT * FROM tempat_sampah")->num_rows();
+        $mobil_sampah = $this->db->query("SELECT * FROM mobil_sampah")->num_rows();
+
+        $this->db->from('list_tugas');
+        $this->db->where('ID_PENGGUNA', $id_pengguna);
+        $tugas = $this->db->get()->num_rows();
+
+        $respon = [
+            'status' => true,
+            'message' => "Data berhasil didapatkan",
+            'home' => [
+                "daftar_lokasi" => "$tempat_sampah",
+                "histori" => "$tugas",
+                "mobil" => "$mobil_sampah",
+            ]
         ];
         $json = json_encode($respon);
         echo $json;
